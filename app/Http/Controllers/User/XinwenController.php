959d2e4ae6a96a8755c09f2ab60a399359f50b5d
tzlinux <87437537@qq.com>
2017-08-14 01:50:43 +0800

    nothing

--- app/Http/Controllers/User/XinwenController.php
+++ app/Http/Controllers/User/XinwenController.php
@@ -12,11 +12,9 @@ class XinwenController extends Controller
 {
     public function addnews(RequestandResponse $request)
     {
-        echo 'sss';die;
         $res = [];
         $res = DB::table('news')->orderBy('id')->paginate(10);
         return tredon_view('user.newsadd',['data'=>$res]);
-
     }
 
     public function news_action()
