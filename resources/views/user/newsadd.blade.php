e55662b9b076124c8820339e2dc103ebcc31ddad
tzlinux <87437537@qq.com>
2017-08-18 01:34:09 +0800

    ss

--- resources/views/user/newsadd.blade.php
+++ resources/views/user/newsadd.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'AdminLTE')
 
 @section('content_header')
-@include('vendor.ueditor.assets')
+
 @stop
 
 @section('content')
@@ -13,20 +13,20 @@
     <body>
     <div>
         <h1>新闻发布</h1>
+        @include('vendor.ueditor.assets')
         <form action="/news_action" method="post">
             新闻标题&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;<input type="text" style="margin-bottom: 20px;width: 500px;" name="title" value=""/>
-            <!-- 实例化编辑器 -->
-            <script type="text/javascript">
-                var ue = UE.getEditor('container');
-                ue.ready(function() {
-                    ue.execCommand('serverparam', '_token', '{{ csrf_token() }}'); // 设置 CSRF token.
-                });
-            </script>
-
             <!-- 编辑器容器 -->
             <script id="container" name="content" type="text/plain"></script>
             <input type="submit"  style="width:100px;" value="提交"/>
         </form>
         </div>
+
+    <script type="text/javascript">
+        var ue = UE.getEditor('container');
+        ue.ready(function() {
+            ue.execCommand('serverparam', '_token', '{{ csrf_token() }}'); // 设置 CSRF token.
+        });
+    </script>
     </body>
 @stop
